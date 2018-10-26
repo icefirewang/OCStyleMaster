552fe46113be843b8b17de64444e58eabaef5845
icefire_wang <icefire_wang@163.com>
2018-10-26 15:32:18 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# Copyright 2013 The OCStyleMaster Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Setup script for OCStyleMaster."""
+
+from setuptools import setup, find_packages
+
+try:
+  from OCStyleMaster import setup
+except ImportError:
+  from distutils.core import setup
+
+
+setup(name='ocstylemaster',
+      version='0.1',
+      description='Objective-C style checker',
+      keywords=('pip','Objective-C','OC','style'),
+      author='icefire_wang, Inc.',
+      author_email = "icefire_wang@163.com",
+      url='https://github.com/icefirewang/OCStyleMaster',
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=[
+
+      ],
+      entry_points={
+        'console_scripts': [
+          'occ = OCStyleMaster.main:main'
+        ]
+      },
+)
